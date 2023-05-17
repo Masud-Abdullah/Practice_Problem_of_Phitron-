fd6e5736447570166b861b07f65a291e1ff4e2f0
Masud Abdullah <abdullahtamim885@gmail.com>
2023-05-17 15:00:31 +0600

    Codeforces Practice Sheet Contest 1 (E)1

--- E_Interval_Sweep.cpp
+++ E_Interval_Sweep.cpp
@@ -1,13 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int a,b;
-    cin>>a>>b;
-    if(a>0 && b>0 &&(a==b || a==b+1 || a+1==b))
+    int a, b;
+    cin >> a >> b;
+    if (a > 0 && b > 0 && (a == b || a == b + 1 || a + 1 == b))
     {
-        cout<<"YES"<<endl;
+        cout << "YES" << endl;
     }
-    else cout<<"NO"<<endl;
+    else
+        cout << "NO" << endl;
+        
     return 0;
 }
